76d4081a444c4596e1124050a8c1e896e415fe84
kassraniroumand <kassra.niroumand@gmail.com>
2024-03-22 16:51:13 +0000

    update

--- untitled13/target/generated-sources/com/stocktracker/springbootgraphql/models/Generated.java
+++
@@ -1,8 +0,0 @@
-package com.stocktracker.springbootgraphql.models;
-
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-
-@Retention(RetentionPolicy.CLASS)
-public @interface Generated {
-}
